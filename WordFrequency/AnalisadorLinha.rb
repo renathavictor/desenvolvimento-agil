b65d3e50a00d772851b3e151bf81863710477a04
Renatha Victor <renatha.nvictor@gmail.com>
2020-09-22 16:44:42 -0300

    cleaning up code

--- WordFrequency/AnalisadorLinha.rb
+++ WordFrequency/AnalisadorLinha.rb
@@ -1,18 +1,15 @@
 class AnalisadorLinha
   
-  attr_accessor :line_number, :line_content, :word_frequency # replace getter and setter
+  attr_accessor :line_number, :line_content, :word_frequency
 
-  def initialize(line_number, line_content) # constructor
+  def initialize(line_number, line_content)
     @line_number = line_number
     @line_content = line_content
   end
 
   def count_frequency_word
-    # converte to array
     to_array = @line_content.scan(/[\wÀ-ú]+/)
-    # to_array = @line_content.split
 
-    # count and add frequency
     @word_frequency = Hash.new(0)
     to_array.each do |word|
       @word_frequency[word] += 1
--- WordFrequency/main.rb
+++ WordFrequency/main.rb
@@ -1,24 +1,22 @@
 require_relative 'AnalisadorLinha'
 
-linhas = IO.readlines('text.txt')
+lines = IO.readlines('text.txt')
 
-instacias_analisador = Array.new
-numero_linha = 0
+instances_array = Array.new
+line_number = 0
 
-linhas.each do |linha|
-  numero_linha += 1
-  instancia = AnalisadorLinha.new(numero_linha, linha)
-  instacias_analisador.push(instancia)
+lines.each do |line|
+  line_number += 1
+  instance = AnalisadorLinha.new(line_number, line)
+  instances_array.push(instance)
 end
 
 
-def palavras_mais_frequencia_linha(array_analisador)
-  # metodo para mostrar as palavras com mais frequencia por linha
-  array_analisador.each do |a|
+def palavras_mais_frequencia_linha(array)
+  array.each do |a|
     a.count_frequency_word
-    # print a::max_frequency_word
   end
 
 end
 
-palavras_mais_frequencia_linha(instacias_analisador)
+palavras_mais_frequencia_linha(instances_array)
